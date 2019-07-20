b31295be138ba6ef8e223e9d1ce5b8260bcaeeab
Ryan Weingart <rmweingart5@gmail.com>
2019-07-20 17:58:58 -0400

    added jquery to obtain inputs from form and transfer to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,16 +8,35 @@
     messagingSenderId: "566144752510",
     appId: "1:566144752510:web:e8a2eaea68268a98"
   };
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  //Variable to reference Firebase
   var database = firebase.database();
 
+  //Initial variables
   var trainName = "";
   var trainDestination = "";
   var trainTime = "";
   var trainFrequency = "";
 
+  //Submit changes what is stored in Firebase
   $("#submit").on("click", function(event) {
-      
-  })
+      //Prevents page from refreshing
+      event.preventDefault();
+
+      //Get inputs from form
+      var trainName = $("#name").val().trim();
+      var trainDestination = $("#destination").val().trim();
+      var trainTime = $("#time").val().trim();
+      var trainFrequency = $("#frequency").val().trim();
+
+      //Change what is saved in Firebase
+      database.ref().push({
+          name: trainName,
+          destination: trainDestination,
+          time: trainTime,
+          frequency: trainFrequency
+      });
+  });
