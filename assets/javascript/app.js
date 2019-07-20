920cdccc40328e6f7c58aec92a2ac3c29761e464
Ryan Weingart <rmweingart5@gmail.com>
2019-07-20 13:41:12 -0400

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDhsN2zudiKguLwM_RrHoH8WaHNYF4O1cE",
+    authDomain: "train-scheduler-c3ca7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c3ca7.firebaseio.com",
+    projectId: "train-scheduler-c3ca7",
+    storageBucket: "",
+    messagingSenderId: "566144752510",
+    appId: "1:566144752510:web:e8a2eaea68268a98"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
