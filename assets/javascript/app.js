c9bb5d311c912488dbacee4b9438dfb7f7cb3004
Ryan Weingart <rmweingart5@gmail.com>
2019-07-23 10:30:39 -0400

    created a function to clear the table so it does not copy inputs from before twice

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,7 @@
   var trainTime = "";
   var trainFrequency = "";
 
+  
   //Submit changes what is stored in Firebase
   $("#submit").on("click", function(event) {
       //Prevents page from refreshing
@@ -30,45 +31,56 @@
       var trainName = $("#name").val().trim();
       var trainDestination = $("#destination").val().trim();
       var trainTime = $("#time").val().trim();
-      var trainFrequency = $("#frequency").val().trim();
+      var frequency = $("#frequency").val().trim();
 
       //Change what is saved in Firebase
       database.ref().push({
           name: trainName,
           destination: trainDestination,
           time: trainTime,
-          frequency: trainFrequency
-      });
-  });
-
-  database.ref().on("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-          
-        var currentTime = moment();
-        console.log("Current Time: " + moment(currentTime).format("hh:mm"));
-
-        var timeDifference = moment().diff(moment(childSnapshot.val().trainTime, "hh:mm"), "minutes");
-        console.log("Difference in Time: " + timeDifference);
-
-        var timeRemainder = timeDifference % childSnapshot.val().frequency;
-        console.log(timeRemainder);
-
-        var timeUntilTrainArrival = childSnapshot.val().frequency - timeRemainder;
-        console.log("Minutes Until Train Arrival: " + timeUntilTrainArrival);
-
-        var nextTrainArrival = moment().add(timeUntilTrainArrival, "minutes");
-        console.log("Arrival Time: " + moment(nextTrainArrival).format("hh:mm"));
-
-        $("table tbody").append('<tr><td>' + childSnapshot.val().name + 
-        '</td><td>' + childSnapshot.val().destination +
-        '</td><td>' + childSnapshot.val().frequency +
-        '</td><td>' + nextTrainArrival.format("hh:mm A") +
-        '</td><td>' + timeUntilTrainArrival +
-        '</td></tr>');
-        console.log(childSnapshot.val());
+          frequency: frequency
       });
 
-      if (snapshot.child("time").exists()) {
-          $("#time").text(snapshot.val().trainTime);
-      }
+      //Clears inputs from form after submit button is clicked
+      for (var i = 0; i < $('form').length; i++) {
+          $('form')[i].reset();
+      };
+      clearTableContent();
+      database.ref().on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+    
+          var currentTime = moment();
+          console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+    
+          var timeDifference = moment().diff(moment(childSnapshot.val().time, "hh:mm"), "minutes");
+          console.log("Difference in Time: " + timeDifference);
+    
+          var timeRemainder = timeDifference % childSnapshot.val().frequency;
+          console.log(timeRemainder);
+    
+          var timeUntilTrainArrival = childSnapshot.val().frequency - timeRemainder;
+          console.log("Minutes Until Train Arrival: " + timeUntilTrainArrival);
+    
+          var nextTrainArrival = moment().add(timeUntilTrainArrival, "minutes");
+          console.log("Arrival Time: " + moment(nextTrainArrival).format("hh:mm"));
+    
+          $("tbody").append('<tr><td>' + childSnapshot.val().name + 
+          '</td><td>' + childSnapshot.val().destination +
+          '</td><td>' + childSnapshot.val().frequency +
+          '</td><td>' + nextTrainArrival.format("hh:mm A") +
+          '</td><td>' + timeUntilTrainArrival +
+          '</td></tr>');
+          console.log(childSnapshot.val());
+        });
+    
+        if (snapshot.child("time").exists()) {
+            $("#time").text(snapshot.val().trainTime);
+        }
+    });
   });
+
+  function clearTableContent () {
+      $('tbody').empty();
+  };
+
+
