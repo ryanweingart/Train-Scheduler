cce99d811c9fe044c582c4d907abea0702b50b83
Ryan Weingart <rmweingart5@gmail.com>
2019-07-24 11:42:55 -0400

    cleaned up code line layout and added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,106 +7,86 @@ var firebaseConfig = {
     storageBucket: "",
     messagingSenderId: "32421656068",
     appId: "1:32421656068:web:e5e290f4d2f17578"
-  };
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  //Variable to reference Firebase
-  var database = firebase.database();
+//Variable to reference Firebase
+var database = firebase.database();
   
-  //Displays the current time
-  setInterval (function() {
-      $(".currentTime").html(moment().format('HH:mm:ss A'))
-  }, 1000);
+//Displays the current time
+setInterval (function() {
+    $(".currentTime").html(moment().format('HH:mm:ss A'))
+}, 1000);
 
-  //Initial variables
-  var trainName = "";
-  var trainDestination = "";
-  var trainTime = "";
-  var trainFrequency = "";
-
-  
-  //Submit changes what is stored in Firebase
-  $("#submit").on("click", function(event) {
-      //Prevents page from refreshing
-      event.preventDefault();
+//Initial variables
+var trainName = "";
+var trainDestination = "";
+var trainTime = "";
+var trainFrequency = "";
 
   
-      //Get inputs from form
-      var trainName = $("#name").val().trim();
-      var trainDestination = $("#destination").val().trim();
-      var trainTime = $("#time").val().trim();
-      var frequency = $("#frequency").val().trim();
+//Submit changes what is stored in Firebase
+$("#submit").on("click", function(event) {
+    //Prevents page from refreshing
+    event.preventDefault();
 
-      //Change what is saved in Firebase
-      database.ref().push({
-          name: trainName,
-          destination: trainDestination,
-          time: trainTime,
-          frequency: frequency
-      });
+    //Get inputs from form
+    var trainName = $("#name").val().trim();
+    var trainDestination = $("#destination").val().trim();
+    var trainTime = $("#time").val().trim();
+    var frequency = $("#frequency").val().trim();
 
-      //Clears the input form after submit button is clicked
-      $("#name").val("");
-      $("#destination").val("");
-      $("#time").val("");
-      $("#frequency").val("");
+    //Change what is saved in Firebase
+    database.ref().push({
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: frequency
+    });
 
-      
-  });
+    //Clears the input form after submit button is clicked
+    $("#name").val("");
+    $("#destination").val("");
+    $("#time").val("");
+    $("#frequency").val("");
+});
 
-  //Function to clear the table to avoid repeating schedules
-  function clearTableContent () {
-      $('tbody').empty();
-  };
+//Function to clear the table to avoid repeating schedules
+function clearTableContent () {
+    $('tbody').empty();
+};
 
-
-
-  database.ref().on("value", function(snapshot) {
+database.ref().on("value", function(snapshot) {
 
     //Clears table to not repeat times
     clearTableContent();
 
     snapshot.forEach(function(childSnapshot) {
+        //Variables to hold child snapshots from Firebase
         var nameSS = childSnapshot.val().name;
         var destinationSS = childSnapshot.val().destination;
         var frequencySS = childSnapshot.val().frequency;
         var timeSS = childSnapshot.val().time;
 
+        //Variable that sets train time back one year to ensure future arrivals
         var trainReset = moment(timeSS, "hh:mm").subtract(1, "y");
 
-      var timeDifference = moment().diff(moment(trainReset, "hh:mm"), "minutes");
-      console.log("Difference in Time: " + timeDifference);
+        //Variables used to calculate how much time until the next train arrives
+        var timeDifference = moment().diff(moment(trainReset, "hh:mm"), "minutes");
+        var timeRemainder = timeDifference % frequencySS;
+        var timeUntilTrainArrival = frequencySS - timeRemainder;
 
-      var timeRemainder = timeDifference % frequencySS;
-      console.log(timeRemainder);
+        //Variable to show what time next train will arrive
+        var nextTrainArrival = moment().add(timeUntilTrainArrival, "minutes");
 
-      var timeUntilTrainArrival = frequencySS - timeRemainder;
-      console.log("Minutes Until Train Arrival: " + timeUntilTrainArrival);
-
-      var nextTrainArrival = moment().add(timeUntilTrainArrival, "minutes");
-      console.log("Arrival Time: " + moment(nextTrainArrival).format("HH:mm"));
-
-    //   if (moment().isSameorBefore(timeSS)) {
-    //       timeUntilTrainArrival = moment(timeSS).diff(moment(), "m");
-    //       nextTrainArrival = moment(timeSS, "hh:mm").format("hh:mm");
-    //   } else {
-    //       timeSS = moment(timeSS).add(frequencySS, "m");
-
-    //   }
-
-      $("tbody").append('<tr><td>' + nameSS + 
-      '</td><td>' + destinationSS +
-      '</td><td>' + frequencySS +
-      '</td><td>' + nextTrainArrival.format("hh:mm A") +
-      '</td><td>' + timeUntilTrainArrival +
-      '</td></tr>');
-      console.log(childSnapshot.val());
+        //jQuery that will append values into new rows
+        $("tbody").append('<tr><td>' + nameSS + 
+        '</td><td>' + destinationSS +
+        '</td><td>' + frequencySS +
+        '</td><td>' + nextTrainArrival.format("hh:mm A") +
+        '</td><td>' + timeUntilTrainArrival +
+        '</td></tr>');
     });
-
-
-    // if (snapshot.child("time").exists()) {
-    //     $("#time").text(snapshot.val().trainTime);
-    // }
 });
